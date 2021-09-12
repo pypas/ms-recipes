9a2a2a686089dccb92b7916a9c6ded369b9b16a0
pypas <pypasofar@gmail.com>
2021-09-12 16:41:16 -0300

    Added GET recipe tests

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -8,6 +8,16 @@ RSpec.describe 'MS Recipes' do
     Sinatra::Application
   end
 
+  let(:recipe) do
+    { 'title' => 'Crispy Chicken and Rice	with Peas & Arugula Salad',
+      # rubocop:disable Layout/LineLength
+      'image_url' => '//images.ctfassets.net/kk2bw5ojx476/5mFyTozvSoyE0Mqseoos86/fb88f4302cfd184492e548cde11a2555/SKU1479_Hero_077-71d8a07ff8e79abcb0e6c0ebf0f3b69c.jpg',
+      'description' => 'Crispy chicken skin, tender meat, and rich, tomatoey sauce form a winning trifecta of delicious in this one-pot braise. We spoon it over rice and peas to soak up every last drop of goodness, and serve a tangy arugula salad alongside for a vibrant boost of flavor and color. Dinner is served! Cook, relax, and enjoy',
+      # rubocop:enable Layout/LineLength
+      'tag' => 'gluten free',
+      'chef' => 'Jony Chives' }
+  end
+
   describe 'GET /' do
     let(:response) { get '/' }
     context 'all recipes' do
@@ -16,13 +26,40 @@ RSpec.describe 'MS Recipes' do
       end
 
       it 'displays title' do
-        expect(response.body).to include('White Cheddar Grilled Cheese with Cherry Preserves & Basil')
+        expect(response.body).to include(recipe['title'])
       end
 
       it 'displays image' do
-        # rubocop:disable Layout/LineLength
-        expect(response.body).to include('//images.ctfassets.net/kk2bw5ojx476/61XHcqOBFYAYCGsKugoMYK/0009ec560684b37f7f7abadd66680179/SKU1240_hero-374f8cece3c71f5fcdc939039e00fb96.jpg')
-        # rubocop:enable Layout/LineLength
+        expect(response.body).to include(recipe['image_url'])
+      end
+    end
+  end
+
+  describe 'GET /:id' do
+    let(:response) { get '/437eO3ORCME46i02SeCW46' }
+    context 'detail recipe' do
+      it 'returns status 200 OK' do
+        expect(response).to be_ok
+      end
+
+      it 'displays title' do
+        expect(response.body).to include(recipe['title'])
+      end
+
+      it 'displays image' do
+        expect(response.body).to include(recipe['image_url'])
+      end
+
+      it 'displays description' do
+        expect(response.body).to include(recipe['description'])
+      end
+
+      it 'displays tag' do
+        expect(response.body).to include(recipe['tag'])
+      end
+
+      it 'displays chef' do
+        expect(response.body).to include(recipe['chef'])
       end
     end
   end
